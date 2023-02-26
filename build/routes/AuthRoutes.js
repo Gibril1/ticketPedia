668759a72816714cb322725dc8a0cf4f80dfb584
Gibril Idrisu Issaka <57862354+Gibril1@users.noreply.github.com>
2023-02-26 14:30:32 -0800

    Delete build directory

--- build/routes/AuthRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const router = require('express').Router();
-const { loginUser, registerUser } = require('../controllers/AuthControllers');
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-module.exports = router;
--- build/routes/UserRoutes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const router = require('express').Router();
-const { buyTicket } = require('../controllers/UserControllers');
-const { protect } = require('../middleware/AuthMiddleware');
-router.post('/buy/:id', protect, buyTicket);
-module.exports = router;
